bbd13f4e9735abb69b7f3eea2038bf35e98b35cc
Miguel Angel Martinez <correomam@gmail.com>
2021-03-22 10:29:50 +0100

    Uso Avatar de PrimeNG.

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,10 +6,12 @@ import { ToastModule } from 'primeng/toast';
 import { CalendarModule } from 'primeng/calendar';
 import { SidebarModule } from 'primeng/sidebar';
 import { TableModule } from 'primeng/table';
+import { AvatarModule } from 'primeng/avatar';
 
 @NgModule({
   declarations: [],
   exports: [
+    AvatarModule,
     SplitButtonModule,
     ButtonModule,
     ToastModule,
