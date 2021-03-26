6a2a1082342eebb2e520c4f7e5b18ff8c706b4bf
Miguel Angel Martinez <correomam@gmail.com>
2021-03-26 09:45:47 +0100

    Mejora tabla añadiendo paginacion.

--- src/app/entities/solicitudes/solicitud.component.ts
+++ src/app/entities/solicitudes/solicitud.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import * as moment from 'moment';
 
@@ -14,6 +14,7 @@ import { ISolicitud } from './solicitud.interface';
 export class SolicitudComponent implements OnInit {
   solicitudes: any[] = [];
   isAdmin = environment.IsAdmin;
+
 
   constructor(protected solicitudService: SolicitudService) { }
 
