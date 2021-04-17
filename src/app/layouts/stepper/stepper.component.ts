653d077c7bc82a154d542e31ad2eb5a665795abc
Miguel Angel Martinez <correomam@gmail.com>
2021-04-17 18:11:15 +0200

    Mejoras en stepper

--- src/app/layouts/stepper/stepper.component.ts
+++ src/app/layouts/stepper/stepper.component.ts
@@ -13,15 +13,22 @@ import { ISolicitud } from '../../entities/solicitudes/solicitud.interface';
 })
 export class StepperComponent implements OnInit {
   solicitud: ISolicitud | null = null;
+  respondida: boolean = false;
+  iniciada: boolean = false
 
-  constructor( protected activatedRoute: ActivatedRoute,) { }
+  constructor(protected activatedRoute: ActivatedRoute,) { }
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ solicitud }) => (this.solicitud = solicitud));
+    if (this.solicitud!.fechaInicio) {
+      this.iniciada = true
+    }
+
     // Calculo días trancurridos desde la solicitud.
     if (this.solicitud!.fechaRespuesta) {
       const date = moment(this.solicitud!.fechaRespuesta);
       this.solicitud!.diasRespuesta = date.diff(this.solicitud!.fechaSolicitud, 'days');
+      this.respondida = true
     } else {
       const date = moment();
       this.solicitud!.diasRespuesta = date.diff(this.solicitud!.fechaSolicitud, 'days');
