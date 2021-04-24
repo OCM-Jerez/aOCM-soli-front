3ba5e9c5703a1009ab1d225780ce66142d06886a
Miguel Angel Martinez <correomam@gmail.com>
2021-04-24 18:38:10 +0200

    Limpio codigo.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ValidarTokenGuard }    from './guards/validar-token.guard';
-import { UserComponent }        from './entities/users/user.component';
-import { SolicitudComponent }   from './entities/solicitudes/solicitud.component';
-import { DocumentoComponent }   from './entities/documentos/documento.component';
-import { GestionComponent }     from './entities/gestiones/gestion.component';
-import { SolicitudDetailComponent } from './entities/solicitudes/solicitud-detail.component';
+import { ValidarTokenGuard } from './guards/validar-token.guard';
+import { UserComponent } from './entities/users/user.component';
 
 const routes: Routes = [
   {
@@ -14,54 +10,33 @@ const routes: Routes = [
   },
   {
     path: 'solicitudes',
-    loadChildren: () => import('./entities/solicitudes/solicitud.module').then( m => m.SolicitudModule ),
-    canActivate: [ ValidarTokenGuard ],
-    canLoad: [ ValidarTokenGuard ],
+    loadChildren: () => import('./entities/solicitudes/solicitud.module').then(m => m.SolicitudModule),
+    canActivate: [ValidarTokenGuard],
+    canLoad: [ValidarTokenGuard],
   },
-  // {
-  //   path: 'solicitudes',
-  //   // loadChildren: () => import('./entities/solicitudes/solicitud.module').then( m => m.SolicitudModule ),
-  //   canActivate: [ ValidarTokenGuard ],
-  //   canLoad: [ ValidarTokenGuard ],
-  //   component: SolicitudComponent,
-  // },
-  // {
-  //   path: 'solicitudes/details',
-  //   // loadChildren: () => import('./entities/solicitudes/solicitud.module').then( m => m.SolicitudModule ),
-  //   canActivate: [ ValidarTokenGuard ],
-  //   canLoad: [ ValidarTokenGuard ],
-  //   component: SolicitudDetailComponent,
-  // },
 
   {
     path: 'documentos',
-    loadChildren: () => import('./entities/documentos/documento.module').then( m => m.DocumentoModule ),
-    canActivate: [ ValidarTokenGuard ],
-    canLoad: [ ValidarTokenGuard ],
+    loadChildren: () => import('./entities/documentos/documento.module').then(m => m.DocumentoModule),
+    canActivate: [ValidarTokenGuard],
+    canLoad: [ValidarTokenGuard],
   },
 
   {
     path: 'gestiones',
-    loadChildren: () => import('./entities/gestiones/gestion.module').then( m => m.GestionModule ),
-    canActivate: [ ValidarTokenGuard ],
-    canLoad: [ ValidarTokenGuard ],
+    loadChildren: () => import('./entities/gestiones/gestion.module').then(m => m.GestionModule),
+    canActivate: [ValidarTokenGuard],
+    canLoad: [ValidarTokenGuard],
   },
 
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
-  // {
-  //   path: 'dashboard',
-  //   loadChildren: () => import('./protected/protected.module').then( m => m.ProtectedModule ),
-  //   canActivate: [ ValidarTokenGuard ],
-  //   canLoad: [ ValidarTokenGuard ]
-  // },
+
   {
     path: '**',
     redirectTo: 'auth'
-    // redirectTo: 'documentos'
-
   }
 ];
 
