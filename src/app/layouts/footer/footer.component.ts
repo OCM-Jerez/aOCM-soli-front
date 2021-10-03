27847deadf8844398b301f1344a36d3c4b71456c
mamjerez <correomam@gmail.com>
2021-10-03 17:08:48 +0200

    Update footer

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 
 // https://github.com/FortAwesome/angular-fontawesome
-import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faFacebook, faGithub, faWordpress } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-   templateUrl: './footer.component.html',
+  templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent  {
+export class FooterComponent {
   fabTwitter = faTwitter;
   fabFacebook = faFacebook;
+  fabGithub = faGithub;
+  faWordpress = faWordpress;
 
   constructor() {
-   }
+  }
 
 }
