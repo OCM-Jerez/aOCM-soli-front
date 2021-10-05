08f03f4f00174d7a9dd75f89777c1feec5d41cc3
mamjerez <correomam@gmail.com>
2021-10-05 21:39:55 +0200

    Delet alñl references to p-splitButton

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { QueryParamsHandling, Router } from '@angular/router';
-
-// import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
 
 import { faUser, faHome, faAtlas, faBell } from '@fortawesome/free-solid-svg-icons';
 
@@ -16,8 +14,6 @@ import { LocalStorageService } from 'ngx-webstorage';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  // entidades: MenuItem[] = [];
-  // opciones: MenuItem[] = [];
 
   isAdmin = environment.IsAdmin;
   userLoged?: string;
@@ -27,10 +23,8 @@ export class NavbarComponent implements OnInit {
   faAtlas = faAtlas;
   faBell = faBell;
 
-  public seletedEntidad: IMenuItemOCM = { id: 0, opcion: '' };
-  public entidades1: IMenuItemOCM[] = []
-  public seletedOpcion: IMenuItemOCM = { id: 0, opcion: '' };
-  public opciones1: IMenuItemOCM[] = []
+  public entidades: IMenuItemOCM[] = []
+  public opciones: IMenuItemOCM[] = []
 
   constructor(
     private loginService: LoginService,
@@ -40,97 +34,51 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.userLoged = this.$localStorage.retrieve('userLog');
-    // this.entidades = [
-    //   {
-    //     label: 'Usuarios', command: () => {
-    //       this.router.navigate(['users']);
-    //     }
-    //   },
-    //   {
-    //     label: 'Solicitudes', command: () => {
-    //       this.router.navigate(['solicitudes']);
-    //     }
-    //   },
-    //   {
-    //     label: 'Documentos', command: () => {
-    //       this.router.navigate(['documentos']);
-    //     }
-    //   },
-    //   {
-    //     label: 'Gestiones', command: () => {
-    //       this.router.navigate(['gestiones']);
-    //     }
-    //   }
-    // ];
 
-    // this.opciones = [
-    //   {
-    //     label: 'Perfil', command: () => {
-    //       console.log('usuarios');
-    //       this.router.navigate(['users/datos']);
-    //     }
-    //   },
-    //   {
-    //     label: 'Contraseña', command: () => {
-    //       this.router.navigate(['solicitudes']);
-    //     }
-    //   },
-    //   {
-    //     label: 'Cambiar usuario', command: () => {
-    //       this.router.navigate(['account/password']);
-    //     }
-    //   },
-    //   {
-    //     label: 'Salir', command: () => {
-    //       this.loginService.logout();
-    //       this.router.navigate(['']);
-    //     }
-    //   }
-    // ];
-
-    this.entidades1 = [
-      { id: 1, opcion: 'Usuarios' },
-      { id: 2, opcion: 'Solicitudes' },
-      { id: 3, opcion: 'Documentos' },
-      { id: 4, opcion: 'Gestiones' },
+    this.entidades = [
+      { id: 0, opcion: 'Usuarios' },
+      { id: 1, opcion: 'Solicitudes' },
+      { id: 2, opcion: 'Documentos' },
+      { id: 3, opcion: 'Gestiones' },
     ];
-    this.opciones1 = [
-      { id: 1, opcion: 'Perfil' },
-      { id: 2, opcion: 'Contraseña' },
-      { id: 3, opcion: 'Cambiar usuario' },
-      { id: 4, opcion: 'Salir' },
+
+    this.opciones = [
+      { id: 0, opcion: 'Perfil' },
+      { id: 1, opcion: 'Contraseña' },
+      { id: 2, opcion: 'Cambiar usuario' },
+      {
+        id: 3, opcion: 'Salir',
+        command: () => {
+          this.loginService.logout();
+          this.router.navigate(['']);
+        }
+      },
     ];
 
   }
 
-  onSelectOption(id: any): void {
-    switch (id.value) {
+  onSelectEntidad(event: any): void {
+    switch (event.value) {
       case '3':
-        console.log(id.value);
-        this.loginService.logout();
-        this.router.navigate(['']);
+
         break;
       case '4':
-        console.log(id.value);
-        this.loginService.logout();
-        this.router.navigate(['']);
+
         break;
       default:
         break;
     }
   }
 
-  onSelectEntidad(id: any): void {
-    switch (id.value) {
-      case '3':
-        console.log(id.value);
-        this.loginService.logout();
-        this.router.navigate(['']);
+  onSelectOption(event: any): void {
+    switch (event.value) {
+      case '2':
+
         break;
-      case '4':
-        console.log(id.value);
-        this.loginService.logout();
-        this.router.navigate(['']);
+      case '3':
+        if (this.opciones[3].command) {
+          this.opciones[3].command();
+        }
         break;
       default:
         break;
@@ -141,40 +89,7 @@ export class NavbarComponent implements OnInit {
 
 interface IMenuItemOCM {
   id: number;
-  opcion: string
+  opcion: string;
+  command?: (event?: any) => void;
 }
 
-// interface IMenuItem {
-//   label?: string;
-//   icon?: string;
-//   command?: (event?: any) => void;
-//   url?: string;
-//   items?: MenuItem[];
-//   expanded?: boolean;
-//   disabled?: boolean;
-//   visible?: boolean;
-//   target?: string;
-//   escape?: boolean;
-//   routerLinkActiveOptions?: any;
-//   separator?: boolean;
-//   badge?: string;
-//   badgeStyleClass?: string;
-//   style?: any;
-//   styleClass?: string;
-//   title?: string;
-//   id?: string;
-//   automationId?: any;
-//   tabindex?: string;
-//   routerLink?: any;
-//   queryParams?: {
-//     [k: string]: any;
-//   };
-//   fragment?: string;
-//   queryParamsHandling?: QueryParamsHandling;
-//   preserveFragment?: boolean;
-//   skipLocationChange?: boolean;
-//   replaceUrl?: boolean;
-//   state?: {
-//     [k: string]: any;
-//   };
-// }
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,27 +1,25 @@
 import { NgModule } from '@angular/core';
 
-import { SplitButtonModule } from 'primeng/splitbutton';
 import { ButtonModule } from 'primeng/button';
-import { ToastModule } from 'primeng/toast';
 import { CalendarModule } from 'primeng/calendar';
 import { RatingModule } from 'primeng/rating';
 import { SidebarModule } from 'primeng/sidebar';
 import { TableModule } from 'primeng/table';
 import { TabViewModule } from 'primeng/tabview';
 import { TimelineModule } from "primeng/timeline"
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [],
   exports: [
-    SplitButtonModule,
     ButtonModule,
-    ToastModule,
     CalendarModule,
     RatingModule,
     SidebarModule,
     TableModule,
     TabViewModule,
     TimelineModule,
+    ToastModule,
   ]
 })
 export class PrimeNgModule { }
