3c01cd782d72600a2ce7b7b5497a1364f7f54237
mamjerez <correomam@gmail.com>
2021-10-05 21:47:04 +0200

    Comment unused primeng modules

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { RatingModule } from 'primeng/rating';
-import { SidebarModule } from 'primeng/sidebar';
+// import { SidebarModule } from 'primeng/sidebar';
 import { TableModule } from 'primeng/table';
 import { TabViewModule } from 'primeng/tabview';
-import { TimelineModule } from "primeng/timeline"
-import { ToastModule } from 'primeng/toast';
+// import { TimelineModule } from "primeng/timeline"
+// import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [],
@@ -15,11 +15,11 @@ import { ToastModule } from 'primeng/toast';
     ButtonModule,
     CalendarModule,
     RatingModule,
-    SidebarModule,
+    // SidebarModule,
     TableModule,
     TabViewModule,
-    TimelineModule,
-    ToastModule,
+    // TimelineModule,
+    // ToastModule,
   ]
 })
 export class PrimeNgModule { }
