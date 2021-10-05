f063a6e071f786cff003328beae6eb990e1290de
mamjerez <correomam@gmail.com>
2021-10-05 07:09:31 +0200

    Change p-splitButton for select

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { QueryParamsHandling, Router } from '@angular/router';
 
 import { MenuItem } from 'primeng/api';
 
@@ -17,7 +17,7 @@ import { LocalStorageService } from 'ngx-webstorage';
 })
 export class NavbarComponent implements OnInit {
   entidades: MenuItem[] = [];
-  opciones: MenuItem[] = [];
+  // opciones: MenuItem[] = [];
 
   isAdmin = environment.IsAdmin;
   userLoged?: string;
@@ -27,14 +27,18 @@ export class NavbarComponent implements OnInit {
   faAtlas = faAtlas;
   faBell = faBell;
 
+
+  public seletedOpcion: IMenuItemOCM = { id: 0, opcion: '' };
+  public opciones1: IMenuItemOCM[] = []
+
   constructor(
     private loginService: LoginService,
     private router: Router,
     private $localStorage: LocalStorageService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-   this.userLoged =  this.$localStorage.retrieve('userLog');
+    this.userLoged = this.$localStorage.retrieve('userLog');
     this.entidades = [
       {
         label: 'Usuarios', command: () => {
@@ -58,30 +62,95 @@ export class NavbarComponent implements OnInit {
       }
     ];
 
-    this.opciones = [
-      {
-        label: 'Perfil', command: () => {
-          console.log('usuarios');
-          this.router.navigate(['users/datos']);
-        }
-      },
-      {
-        label: 'Contraseña', command: () => {
-          this.router.navigate(['solicitudes']);
-        }
-      },
-      {
-        label: 'Cambiar ususario', command: () => {
-          this.router.navigate(['account/password']);
-        }
-      },
-      {
-        label: 'Salir', command: () => {
-          this.loginService.logout();
-          this.router.navigate(['']);
-        }
-      }
+    // this.opciones = [
+    //   {
+    //     label: 'Perfil', command: () => {
+    //       console.log('usuarios');
+    //       this.router.navigate(['users/datos']);
+    //     }
+    //   },
+    //   {
+    //     label: 'Contraseña', command: () => {
+    //       this.router.navigate(['solicitudes']);
+    //     }
+    //   },
+    //   {
+    //     label: 'Cambiar usuario', command: () => {
+    //       this.router.navigate(['account/password']);
+    //     }
+    //   },
+    //   {
+    //     label: 'Salir', command: () => {
+    //       this.loginService.logout();
+    //       this.router.navigate(['']);
+    //     }
+    //   }
+    // ];
+
+    this.opciones1 = [
+      { id: 1, opcion: 'Perfil' },
+      { id: 2, opcion: 'Contraseña' },
+      { id: 3, opcion: 'Cambiar usuario' },
+      { id: 4, opcion: 'Salir' },
     ];
+
+  }
+
+  onSelect(id: any): void {
+    switch (id.value) {
+      case '3':
+        console.log(id.value);
+        this.loginService.logout();
+        this.router.navigate(['']);
+        break;
+      case '4':
+        console.log(id.value);
+        this.loginService.logout();
+        this.router.navigate(['']);
+        break;
+      default:
+        break;
+    }
   }
 
 }
+
+interface IMenuItemOCM {
+  id: number;
+  opcion: string
+}
+
+// interface IMenuItem {
+//   label?: string;
+//   icon?: string;
+//   command?: (event?: any) => void;
+//   url?: string;
+//   items?: MenuItem[];
+//   expanded?: boolean;
+//   disabled?: boolean;
+//   visible?: boolean;
+//   target?: string;
+//   escape?: boolean;
+//   routerLinkActiveOptions?: any;
+//   separator?: boolean;
+//   badge?: string;
+//   badgeStyleClass?: string;
+//   style?: any;
+//   styleClass?: string;
+//   title?: string;
+//   id?: string;
+//   automationId?: any;
+//   tabindex?: string;
+//   routerLink?: any;
+//   queryParams?: {
+//     [k: string]: any;
+//   };
+//   fragment?: string;
+//   queryParamsHandling?: QueryParamsHandling;
+//   preserveFragment?: boolean;
+//   skipLocationChange?: boolean;
+//   replaceUrl?: boolean;
+//   state?: {
+//     [k: string]: any;
+//   };
+// }
